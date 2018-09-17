83565fcd07eb47cb9f4e4666ac2e4bb346346399
emin <emindedic@gmail.com>
2018-09-17 14:48:22 +0200

    Added pipe to angular app for product code name

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 
@@ -8,7 +8,7 @@ import { IProduct } from "./product";
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -71,4 +71,9 @@ export class ProductListComponent {
       toggleImage(): void {
           this.showImage = !this.showImage;
       }
+
+
+      ngOnInit(): void {
+        console.log('in OnInit');
+    }
 }
